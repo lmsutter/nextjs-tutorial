fd7e41427bac1a635d5d97b1cfcc31c869c20f97
lucass <lucass@marvin.com>
2024-02-05 16:06:31 -0600

    update workign

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,6 @@
+import DashboardSkeleton from "../../ui/skeletons"
+// loading is automatically displayed with the dashboard route is loading.
+
+export default function Loading() {
+    return  <DashboardSkeleton />
+}
